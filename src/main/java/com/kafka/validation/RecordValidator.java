ac211ddefcb911effebd1333a69b350f8e430f8e
mykhailobaluta <mihun@ukr.net>
2018-02-21 15:22:43 +0200

    log 1 message per TopicPartition instead of Record

--- src/main/java/com/kafka/validation/RecordValidator.java
+++ src/main/java/com/kafka/validation/RecordValidator.java
@@ -12,31 +12,36 @@ public class RecordValidator {
 
     public void validate(List<ConsumerRecord> backupConsumerRecords, List<ConsumerRecord> productionConsumerRecords){
         checkConsistence(backupConsumerRecords, productionConsumerRecords);
-        checkRecords(backupConsumerRecords, productionConsumerRecords);
+        int diffCount = checkRecords(backupConsumerRecords, productionConsumerRecords);
+        if (diffCount > 0) {
+            log.error("[{}] There are [{}] different records from [{}]", Thread.currentThread().getName(), diffCount, backupConsumerRecords.size() );
+        }
     }
 
     private void checkConsistence(List<ConsumerRecord> backupConsumerRecords, List<ConsumerRecord> productionConsumerRecords) {
         int backupRecordsSize = backupConsumerRecords.size();
         int productionRecordsSize = productionConsumerRecords.size();
         if (backupRecordsSize > productionRecordsSize){
-            log.error("backupRecords {} more than  productionRecords {}", backupRecordsSize,  productionRecordsSize);
+            log.error("[{}] backupRecords {} more than  productionRecords {}", Thread.currentThread().getName(), backupRecordsSize,  productionRecordsSize);
         } else if (backupRecordsSize < productionRecordsSize) {
-            log.info("backupRecords {} less than  productionRecords {}", backupRecordsSize,  productionRecordsSize);
+            log.info("[{}] backupRecords {} less than  productionRecords {}", Thread.currentThread().getName(), backupRecordsSize,  productionRecordsSize);
         }
     }
 
-    private void checkRecords(List<ConsumerRecord> backupConsumerRecords, List<ConsumerRecord> productionConsumerRecords) {
-        for (int i = 0; i < backupConsumerRecords.size(); i++) {
-            if (i >= productionConsumerRecords.size()) return;
-            checkRecord(backupConsumerRecords.get(i), productionConsumerRecords.get(i));
+    private int checkRecords(List<ConsumerRecord> backupConsumerRecords, List<ConsumerRecord> productionConsumerRecords) {
+        int diffRecordCount = 0;
+        int actualSizeToCheck = Math.min(backupConsumerRecords.size(), productionConsumerRecords.size());
+        for (int i = 0; i < actualSizeToCheck; i++) {
+            if (!isRecordSame(backupConsumerRecords.get(i), productionConsumerRecords.get(i))){
+                diffRecordCount++;
+            }
         }
+        return diffRecordCount;
     }
 
-    private void checkRecord(ConsumerRecord backupRecord, ConsumerRecord productionRecord) {
+    private boolean isRecordSame(ConsumerRecord backupRecord, ConsumerRecord productionRecord) {
         int backupSerializedValueSize = backupRecord.serializedValueSize();
         int productionSerializedValueSize = productionRecord.serializedValueSize();
-        if (backupSerializedValueSize != productionSerializedValueSize){
-            log.error("backupRecord {} is not equals to productionRecord {}", backupRecord, productionRecord);
-        }
+        return backupSerializedValueSize == productionSerializedValueSize;
     }
 }
